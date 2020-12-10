249a2fbd3de4752215548ab222b8ea11c3ca36ad
Nikola Hristov Kalamov <niki122121@gmail.com>
2020-12-10 20:44:26 +0100

    Sistema optimizacion Rooms + tracking de Player
    Se maneja cuando el jugador entra en room nueva
    Nueva opcion para solo mostrar la room en la que se encuentra el jugador (opcionalmente tambien sus rooms vecinas)

---
+++ Assets/Scripts/PlayerTracker.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour
+{
+
+    [SerializeField] LevelController controller;
+    Vector2Int playerGridPos;
+    Vector2Int previousGridPos;
+    Vector2Int enterDir;
+    public List<int> activeRooms;
+    int currentRoom;
+    int previousRoom;
+
+    void Start()
+    {
+        playerGridPos = new Vector2Int(0, 0);
+        previousGridPos = new Vector2Int(0, 0);
+        enterDir = new Vector2Int(0, 0);
+        activeRooms = new List<int>(4);
+        currentRoom = 0;
+        previousRoom = 0;
+    }
+    void Update()
+    {
+        playerGridPos.x = Mathf.RoundToInt(transform.position.x / LevelController.baseRoomeSize);
+        playerGridPos.y = Mathf.RoundToInt(transform.position.z / LevelController.baseRoomeSize);
+
+
+        if (playerGridPos != previousGridPos)
+        {
+            enterDir = playerGridPos - previousGridPos;
+            controller.ocupiedSpaces.TryGetValue(playerGridPos, out currentRoom);
+            if (currentRoom != previousRoom)
+            {
+                print("Entered new Room (" + currentRoom + ") with dir: (" + enterDir + ")" + "   prev:  " + previousRoom);
+            }
+            if (controller.optimization)
+            {
+                controller.roomArray[currentRoom].SetActive(true);
+                if (!controller.showExtraRooms)
+                {
+                    controller.roomArray[previousRoom].SetActive(false);
+                }
+                else
+                {
+                    int aux;
+                    Vector2Int vecAux;
+                    bool foundValue;
+                    activeRooms.Clear();
+
+                    aux = 0;
+                    vecAux = new Vector2Int(1, 0);
+                    foundValue = controller.ocupiedSpaces.TryGetValue(playerGridPos + vecAux, out aux);
+                    if (foundValue && controller.roomMatrix[currentRoom, aux] != 0)
+                    {   
+                        controller.roomArray[aux].SetActive(true);
+                        activeRooms.Add(aux);
+                    }
+                    aux = 0;
+                    vecAux = new Vector2Int(-1, 0);
+                    foundValue = controller.ocupiedSpaces.TryGetValue(playerGridPos + vecAux, out aux);
+                    if (foundValue && controller.roomMatrix[currentRoom, aux] != 0)
+                    {
+                        controller.roomArray[aux].SetActive(true);
+                        activeRooms.Add(aux);
+                    }
+                    aux = 0;
+                    vecAux = new Vector2Int(0, 1);
+                    foundValue = controller.ocupiedSpaces.TryGetValue(playerGridPos + vecAux, out aux);
+                    if (foundValue && controller.roomMatrix[currentRoom, aux] != 0)
+                    {
+                        controller.roomArray[aux].SetActive(true);
+                        activeRooms.Add(aux);
+                    }
+                    aux = 0;
+                    vecAux = new Vector2Int(0, -1);
+                    foundValue = controller.ocupiedSpaces.TryGetValue(playerGridPos + vecAux, out aux);
+                    if (foundValue && controller.roomMatrix[currentRoom, aux] != 0)
+                    {
+                        controller.roomArray[aux].SetActive(true);
+                        activeRooms.Add(aux);
+                    }
+
+                    aux = 0;
+                    vecAux = new Vector2Int(1, 0);
+                    foundValue = controller.ocupiedSpaces.TryGetValue(previousGridPos + vecAux, out aux);
+                    if (foundValue && vecAux != enterDir && !activeRooms.Contains(aux))
+                    {
+                        controller.roomArray[aux].SetActive(false);
+                    }
+                    aux = 0;
+                    vecAux = new Vector2Int(-1, 0);
+                    foundValue = controller.ocupiedSpaces.TryGetValue(previousGridPos + vecAux, out aux);
+                    if (foundValue && vecAux != enterDir && !activeRooms.Contains(aux))
+                    {
+                        controller.roomArray[aux].SetActive(false);
+                    }
+                    aux = 0;
+                    vecAux = new Vector2Int(0, 1);
+                    foundValue = controller.ocupiedSpaces.TryGetValue(previousGridPos + vecAux, out aux);
+                    if (foundValue && vecAux != enterDir && !activeRooms.Contains(aux))
+                    {
+                        controller.roomArray[aux].SetActive(false);
+                    }
+                    aux = 0;
+                    vecAux = new Vector2Int(0, -1);
+                    foundValue = controller.ocupiedSpaces.TryGetValue(previousGridPos + vecAux, out aux);
+                    if (foundValue && vecAux != enterDir && !activeRooms.Contains(aux))
+                    {
+                        controller.roomArray[aux].SetActive(false);
+                    }
+
+
+                    controller.roomArray[currentRoom].SetActive(true);
+                    controller.roomArray[previousRoom].SetActive(true);
+
+                }
+            }
+        }
+        previousGridPos.x = playerGridPos.x;
+        previousGridPos.y = playerGridPos.y;
+
+        previousRoom = currentRoom;
+    }
+}
