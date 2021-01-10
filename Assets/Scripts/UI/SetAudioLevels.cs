a8aa842f3026cfa776d3db333d63f296487874a7
Seven <sevenwordsmusic@gmail.com>
2021-01-10 06:46:29 +0100

    SW_UI_0100A
    -CHECKPOINT.
    -Audio values stored.
    -Control schemes.

--- Assets/Scripts/UI/SetAudioLevels.cs
+++ Assets/Scripts/UI/SetAudioLevels.cs
@@ -3,23 +3,19 @@ using System.Collections;
 using UnityEngine.UI;
 using FMOD.Studio;
 
-
+//AUDIO by @sevenwordsmusic - Got it memorized?
 public class SetAudioLevels : MonoBehaviour {
 
     private FMOD.Studio.Bus bgmBus;
     private FMOD.Studio.Bus sfxBus;
 
-
-    //AUDIO by @sevenwordsmusic
     public void SetMusicLevel(float musicLvl)
 	{
 
         bgmBus=FMODUnity.RuntimeManager.GetBus("bus:/BGM_bus");
-        Debug.Log(bgmBus);
         bgmBus.setVolume(musicLvl);
     }
 
-	//AUDIO by @sevenwordsmusic
 	public void SetSfxLevel(float sfxLevel)
 	{
         sfxBus=FMODUnity.RuntimeManager.GetBus("bus:/SFX_bus");
