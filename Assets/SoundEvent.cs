72f21be61eb5539523b9fc38a70f9dcc05d4e298
Seven <sevenwordsmusic@gmail.com>
2021-01-12 22:15:07 +0100

    SW_AUDIO_0101C
    -Footsteps.

---
+++ Assets/SoundEvent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+using System.Collections;
+
+//@sevenwordsmusic original code :D !!!
+
+public class SoundEvent : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string stepEvent;
+    private FMOD.Studio.EventInstance eventToPlay;
+
+    private bool isIdle=true;
+
+    public void yesIdle()
+    {
+        isIdle = true;
+    }
+
+    public void noIdle()
+    {
+        isIdle = false;
+    }
+
+    public void playStep()
+    {
+        if (!isIdle)
+        {
+            eventToPlay = FMODUnity.RuntimeManager.CreateInstance(stepEvent);
+            eventToPlay.start();
+        }
+
+    }
+}
