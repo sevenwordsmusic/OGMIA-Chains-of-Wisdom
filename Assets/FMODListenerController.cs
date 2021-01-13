117527209cac6b38bea1061537dc611cbc0f3e3c
Víctor Sierra Fernández <vicsierra76@gmail.com>
2021-01-13 01:34:06 +0100

    Mobile buttons handle + base listener controller script

--- Assets/FMODListenerController.cs
+++ Assets/FMODListenerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class FMODListenerController : MonoBehaviour
 {
+    [SerializeField] GameObject player;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(player != null)
+        {
+            this.transform.position = player.transform.position;
+        } 
+        else
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
     }
 }
